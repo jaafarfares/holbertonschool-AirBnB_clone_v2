b95221755c395bcfe8642fb2950175b283b252d4
jaafarfares <jaafarferes@gmail.com>
2022-08-10 14:55:44 +0100

    user class

--- models/user.py
+++ models/user.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel, Base
+
+from models import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
 
 class User(BaseModel, Base):
     """User inherits from BaseModel and Base (respect the order)
     Add or replace in the class User """
+    __tablename__ = "users"
 
-    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", cascade="all,delete", backref='user')
-    reviews = relationship("Review", cascade="all,delete", backref='user')
+    places = relationship("Place", backref="user",
+                          cascade="all, delete-orphan")
+    reviews = relationship("Review", backref="user",
+                           cascade="all, delete-orphan")
