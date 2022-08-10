dca97e466ea96acb99ec149e89575be2c9643da3
jaafarfares <jaafarferes@gmail.com>
2022-08-10 12:44:38 +0100

    User inherits from BaseModel and Base (respect the order) Add or replace in the class User

--- models/user.py
+++ models/user.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""This is the user class"""
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
+class User(BaseModel, Base):
+    """User inherits from BaseModel and Base (respect the order)
+    Add or replace in the class User """
+
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", cascade="all,delete", backref='user')
+    reviews = relationship("Review", cascade="all,delete", backref='user')
