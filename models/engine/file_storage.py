224b3a532f6df58a09ce640c191d55b736466cb2
jaafarfares <jaafarferes@gmail.com>
2022-08-10 15:07:03 +0100

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,13 @@
 
 import json
 import models
+from models.state import State
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.review import Review
+from models.amenity import Amenity
 
 
 class FileStorage:
