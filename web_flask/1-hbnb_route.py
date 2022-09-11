aa447edda2ed98c060f8d40a7ff8b9689b62bd88
jaafarfares <jaafarferes@gmail.com>
2022-09-11 11:48:23 +0100

    start a web application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """
-start a web application using flask
+starts a web applicatin using flask
 """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def home():
     """
-    the messsage needed to display
+        the messsage needed to display
     """
     return 'Hello HBNB!'
 
 
-@app.route('/<hbnb>', strict_slashes=False)
-def HBNB(hbnb):
+@app.route('/hbnb', strict_slashes=False)
+def hbnb1():
     """
-    the messsage needed to display
+        the messsage needed to display
     """
     return 'HBNB'
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
