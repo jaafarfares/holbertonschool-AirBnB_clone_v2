82956efd832fbda2366ed1d083ce89e9a6712054
jaafarfares <jaafarferes@gmail.com>
2022-09-11 11:54:51 +0100

    start a web application using flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,34 +1,36 @@
 #!/usr/bin/python3
 """
-start a web application using flask
+starts a web application using Flask
 """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def home():
     """
-    the messsage needed to display
+        the messsage needed to display
     """
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
-@app.route('/<hbnb>', strict_slashes=False)
-def hbnb(hbnb):
+@app.route('/hbnb', strict_slashes=False)
+def hbnb1():
     """
-    the messsage needed to display
+        the messsage needed to display
     """
-    return "HBNB"
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def texts(text):
+def hbnb2(text):
     """
-    the messsage needed to display
+        the messsage needed to display
     """
-    return "C " + text.replace('_', ' ')
+    return 'C ' + text.replace('_', ' ')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
