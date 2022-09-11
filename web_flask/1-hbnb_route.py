771f09ffbe9d78b100c039c101b82b13263540d1
jaafarfares <jaafarferes@gmail.com>
2022-09-11 11:39:30 +0100

    start a web application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,12 +12,15 @@ def home():
     the messsage needed to display
     """
     return 'Hello HBNB!'
+
+
 @app.route('/<hbnb>', strict_slashes=False)
 def hbnb(hbnb):
     """
     the messsage needed to display
     """
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
